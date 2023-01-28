142186f26aaf6b73fb836a7b5ceaf32feb8e1386
Pablo Diaz <pablo.diazv@pucp.edu.pe>
2023-01-28 19:25:17 -0400

    change versions to replicate error nodejs

--- setup/generator.sh
+++ setup/generator.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
-echo "==========================================\n Reading build.yaml \n=========================================="
-echo "[WARNING] This script is executed inside docker, don't expect working on host"
+printf "\n========================================= Reading build.yaml =========================================\n";
+printf "[WARNING] This script is executed inside docker, don't expect working on host";
 # Read the YAML file
 yaml_contents=$(cat ./setup/generator.yaml)
 
 if [ -z "$yaml_contents" ]; then
-    echo "No build.yaml file found"
+    printf "No build.yaml file found"
     exit 1
 fi
 
-echo "==========================================\n Reading environment variables from build.yaml \n=========================================="
+printf "\n========================================= Reading environment variables from build.yaml =========================================\n";
 # Extract the items array from environment and export it
 keys=$(yq '.environment' ./setup/generator.yaml)
 
@@ -18,36 +18,37 @@ keys=$(yq '.environment' ./setup/generator.yaml)
 while read -r key value; do
     key=${key%\\}
     key=${key%?}
-    echo "Exporting $key=$value"
+    printf "Exporting $key=$value"
     export "$key=$value"
 done <<<"$keys"
 
-echo "==========================================\n Environment variables exported \n=========================================="
+printf "\n========================================= Environment variables exported =========================================\n";
 printenv | grep 'PROTOGENERATOR_'
 
-echo "==========================================\n Compiling the project... \n=========================================="
+printf "\n========================================= Compiling the project... =========================================\n";
+
 # Compile the project
 list_languages=$(yq '.languages[]' ./setup/generator.yaml)
 
-echo "[WARNING] This script is executed inside docker, don't expect working on host"
+printf "\n[WARNING] This script is executed inside docker, don't expect working on host\n";
 for language in $list_languages; do
 
-    echo "Compiling for $language"
+    printf "Compiling for $language\n";
     # Extract the items array from environment and export it
     output=$(yq '.job.'$language'.output' ./setup/generator.yaml)
-    echo "$language.output: $output"
+    printf "$language.output: $output"
     option=$(yq '.job.'$language'.options' ./setup/generator.yaml)
-    echo "$language.options: $option"
+    printf "$language.options: $option"
     verbose=$(yq '.job.'$language'.verbose' ./setup/generator.yaml)
-    echo "$language.verbose: $verbose"
+    printf "$language.verbose: $verbose"
     # Create output dir recursively
     mkdir -p $output
     # Build with protoc
-    echo "Command: find . -name \*.proto -exec protoc $option $1 {} \;"
+    printf "Command: find . -name \*.proto -exec protoc $option $1 {} \;  \n";
     find . -name \*.proto -exec protoc $option $1 {} \;
 
     if [ ! -z "$verbose" ]; then
-        echo "==========================================\n Files compiled inside container \n=========================================="
+        printf "\n========================================= Files compiled inside container =========================================\n";
         tree -aC $output
     fi
 
